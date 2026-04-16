661e3d960655cb6d59f5b058bd2ea2262a47bb04
Sanjay Dhavanam <dhavanamsanjay@gmail.com>
2026-04-17 02:08:40 +0530

    Updated your API URL

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
     const getAllThreads = async () => {
 
         try{
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch("http://sigmagpt-ioc5.onrender.com/api/thread");
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
             console.log(filteredData);
@@ -37,7 +37,7 @@ export default function Sidebar() {
         setCurrThreadId(newThreadId);
 
         try{
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`)
+            const response = await fetch(`http://sigmagpt-ioc5.onrender.com/api/thread/${newThreadId}`)
             const res = await response.json();
             console.log(res);
             setPrevChats(res);
@@ -50,7 +50,7 @@ export default function Sidebar() {
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const response = await fetch(`http://sigmagpt-ioc5.onrender.com/api/thread/${threadId}`, {method: "DELETE"});
             const res = await response.json();
             console.log(res);
 
